c73e44e9712ad82e2406aec08a657686a6d275b3
shelawd <shelawdya44@gmail.com>
2024-05-02 23:41:41 +0700

    commit ketiga

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import Home from "./pages/Home";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store"; 
 import Login from "./pages/Login";
-import DetailPage from "./pages/DetailPage"
+import DetailPage from "./pages/DetailPage";
 import TafsirSurat from "./pages/TafsirSurat";
 import Register from "./pages/Register";
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={ <Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Home />} />
-          <Route path={"/detail-surat/:nomorSurat"} element={<DetailPage />} />
-          <Route path={"/tafsir-surat/:nomorSurat/:nomorAyat"} element={<TafsirSurat />} />
-        </Routes>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/detail-surat/:nomorSurat" element={<DetailPage />} />
+            <Route path="/tafsir-surat/:nomorSurat/:nomorAyat" element={<TafsirSurat />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { thunk } from "redux-thunk";
 import rootReducers from "./reducers";
+import storage from "redux-persist/lib/storage";
+import {persistStore, persistReducer} from "redux-persist";
 
-export default configureStore({
-    reducer: rootReducers,
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["auth"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+export const store = configureStore({
+    reducer: persistedReducer,
     devTools: import.meta.env.NODE_ENV === "development",
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false,}).concat(thunk),
 });
+
+export const persistor = persistStore(store);
