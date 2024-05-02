681ac0b41a970e750037fd897ffc401fb06e5cc6
shelawd <shelawdya44@gmail.com>
2024-05-02 23:23:31 +0700

    commit kedua

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,5 +4,6 @@ import rootReducers from "./reducers";
 
 export default configureStore({
     reducer: rootReducers,
-    devTools: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+    devTools: import.meta.env.NODE_ENV === "development",
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
