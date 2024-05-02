7fdcc1a6c918609a7b608b96cf25c0514aee4875
shelawd <shelawdya44@gmail.com>
2024-05-02 23:48:25 +0700

    nambah onchange di register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -81,6 +81,7 @@ function Register() {
         <div className="flex items-center justify-between">
           <button
             type="submit"
+            onChange={handleSubmit}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
             Register
