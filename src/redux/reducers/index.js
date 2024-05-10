6cf99a10a8cd06145bc636b5653e4950d9bd3890
shelawd <shelawdya44@gmail.com>
2024-05-10 21:01:30 +0700

    commit

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import suratReducers from "./suratReducers";
-import audioReducers from "./audioPlayReducers";
 import authReducers from "./authReducers";
 
 export default combineReducers({
   surat: suratReducers,
   auth: authReducers,
-  audio: audioReducers,
 });
